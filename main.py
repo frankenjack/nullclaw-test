b0da854a3d3b52d6232d14007c582bc205472779
frankenjack <db828128@gmail.com>
2026-03-20 08:08:13 +0700

    test1

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "Hello, Zaki! FastAPI is running."}
+
+@app.get("/test")
+def read_test():
+    return {"message": "Test endpoint is working!"}
